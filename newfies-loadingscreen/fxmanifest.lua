21ed1cf4e0f51b3ac1d1d04fbb0227b9e11011b7
PrivacySecured <LoveNewfies@proton.me>
2025-01-30 12:40:29 -0600

    Update fxmanifest.lua

--- newfies-loadingscreen/fxmanifest.lua
+++ newfies-loadingscreen/fxmanifest.lua
@@ -1,7 +1,7 @@
 version '1.0.5'
 author 'Newfies'
 description 'Newfies plugin that displays a custom loading screen when joining the server on FiveM'
-repository 'https://github.com/newfies/5m-loadingscreen'
+repository 'https://github.com/Newfies/fivem-mini-mods/tree/main/newfies-loadingscreen'
 
 files {
     'index.html',
