318d788c614714b956282316943680687d2c996a
PrivacySecured <LoveNewfies@proton.me>
2025-01-30 12:40:09 -0600

    s

---
+++ newfies-loadingscreen/fxmanifest.lua
@@ -0,0 +1,17 @@
+version '1.0.5'
+author 'Newfies'
+description 'Newfies plugin that displays a custom loading screen when joining the server on FiveM'
+repository 'https://github.com/newfies/5m-loadingscreen'
+
+files {
+    'index.html',
+    'style.css',
+    'bankgothic.ttf',
+    'Pricedown.ttf',
+    'loadingbg.png'
+}
+
+loadscreen 'index.html'
+
+fx_version 'bodacious'
+game 'gta5'
